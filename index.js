6880b88d7fe428b565d63e97f2ff39884087ec41
Rami Chaar <99862145+RamiChaar@users.noreply.github.com>
2025-01-10 17:26:18 -0800

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+// Create the server
+const server = http.createServer((req, res) => {
+    // Set response header
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // Send "Hello World" response
+    res.end('Hello World\n');
+});
+
+// Define the port
+const PORT = 3033
+
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
